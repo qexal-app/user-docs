7fb23ff1312e68aa3c20f2adf0eed747058fdc5f
Vitalii Mural <vitalii.mural@gmail.com>
2023-11-25 18:34:59 +0200

    add auto deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: 'https://docs.qexal.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/user-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'qexal-app', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'user-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
